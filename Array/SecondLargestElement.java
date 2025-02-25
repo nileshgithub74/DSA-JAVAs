03dcc36fc25b44b26ae0ec5c7482e2049c13ca08
Nilesh kumar <kumarnilesh843127@gmail.com>
2025-02-25 11:08:16 +0530

    jaava

--- Array/SecondLargestElement.java
+++ Array/SecondLargestElement.java
@@ -36,28 +36,28 @@
 
 
 
-class SecondLargestELement{
-    public static void main(String[] args) {
-        int[] arr = {2,4,5,3,453,43,53};
+// class SecondLargestELement{
+//     public static void main(String[] args) {
+//         int[] arr = {2,4,5,3,453,43,53};
 
-        int max1 = arr[0];
-        int max2 = arr[0];
+//         int max1 = arr[0];
+//         int max2 = arr[0];
 
-        for(int i =1; i< arr.length; i++ ){
-            if(arr[i] > max1 ){
-                max2 = max1;
-                max1 = arr[i];
+//         for(int i =1; i< arr.length; i++ ){
+//             if(arr[i] > max1 ){
+//                 max2 = max1;
+//                 max1 = arr[i];
                 
-            }else if(arr[i]> max2){
-                max2 = arr[i];
-            }
-        }
+//             }else if(arr[i]> max2){
+//                 max2 = arr[i];
+//             }
+//         }
 
-        System.out.println(max2);
+//         System.out.println(max2);
 
 
         
-}
+// }
 
 
-}
+// }
