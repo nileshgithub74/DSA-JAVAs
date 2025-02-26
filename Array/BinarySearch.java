88b9e291b198c79345be7aa1127e1548509ddcdb
Nilesh kumar <kumarnilesh843127@gmail.com>
2025-02-26 14:22:57 +0530

    java

---
+++ Array/BinarySearch.java
@@ -0,0 +1,10 @@
+ 
+ 
+ class BinarySearch{
+    public static void main(String[] args) {
+
+        
+       
+        
+    }
+ }
---
+++ Array/Searching.java
@@ -0,0 +1,16 @@
+public class Searching {
+    public static void main(String[] args){
+         int arr[] = {1,2,3,5,9};
+         int target = 5;
+
+         for(int i=0; i< arr.length; i++){
+            if(arr[i] == target){
+                System.out.println(  "Element found at"+ i +"value is"+ arr[i]);
+
+            }
+         }
+        
+    }
+
+    
+}
