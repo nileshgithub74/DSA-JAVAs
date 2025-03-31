0b9c791d2d8f3e4fee4c34d692cf284305d44e48
Nilesh kumar <kumarnilesh843127@gmail.com>
2025-03-31 22:33:19 +0530

    java

--- Collections/Arraylist.java/ArrayListDemo.java
+++ Collections/Arraylist.java/ArrayListDemo.java
@@ -1,13 +1,34 @@
-// // package Collections.Arraylist.java;
-// import java.util.*;
+// package Collections.Arraylist.java;
+import java.util.*;
 
-// public class ArrayListDemo {
-//     public static void main(String[] args){
-//         ArrayList<Integer> arrList = new ArrayList<>();
+public class ArrayListDemo {
+    public static void main(String[] args){
+        ArrayList<Integer> arrList = new ArrayList<>();
+
+        arrList.add(12);
+        arrList.add(42);
+        arrList.add(62);
+        arrList.add(52);
+
+        arrList.set(1,60);
+        System.out.println(arrList.indexOf(52));
+      
+
+        // System.out.println(arrList);
+
+        // for(int x : arrList){
+        //     System.out.println(x);
+        // }
+
+        ListIterator<Integer> it =  arrList.listIterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
         
 
 
 
-//     }
+    }
     
-// }
+}
