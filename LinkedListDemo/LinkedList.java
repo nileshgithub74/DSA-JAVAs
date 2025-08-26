d074ff416b9f01477b16ee55403901c04fbe6854
Nilesh Kumar <kumarnilesh843127@gmail.com>
2025-08-26 14:03:56 +0530

    commit

--- LinkedListDemo/LinkedList.java
+++ LinkedListDemo/LinkedList.java
@@ -1,57 +1,57 @@
-package LinkedListDemo;
+// package LinkedListDemo;
 
-class Node{
-  int data;
-  Node next;
-  public Node(int data){
-    this.data = data;
-    this.next =null; 
-  }
-}
+// class Node{
+//   int data;
+//   Node next;
+//   public Node(int data){
+//     this.data = data;
+//     this.next =null; 
+//   }
+// }
 
-public class LinkedList {
+// public class LinkedList {
 
-  static void Traversal(Node head){
-    int count =0;
-    Node curr   = head;
+//   static void Traversal(Node head){
+//     int count =0;
+//     Node curr   = head;
 
-    while(curr != null){
-      System.out.print(curr.data+" ");
-      curr = curr.next;
-          }
-  }
+//     while(curr != null){
+//       System.out.print(curr.data+" ");
+//       curr = curr.next;
+//           }
+//   }
 
 
-  static int NodeCount(Node head){
-    int count =0;
-    Node curr   = head;
-    while(curr != null){
+//   static int NodeCount(Node head){
+//     int count =0;
+//     Node curr   = head;
+//     while(curr != null){
 
-       count++;
-       curr = curr.next;
-    }
+//        count++;
+//        curr = curr.next;
+//     }
 
-    return count;
+//     return count;
     
-  }
+//   }
  
 
-  public static void main(String[] args){
-    Node head = new Node(10);
-    Node a = new Node(20);
-    Node b = new Node(30);
-    head.next = a;
-    a.next = b;
+//   public static void main(String[] args){
+//     Node head = new Node(10);
+//     Node a = new Node(20);
+//     Node b = new Node(30);
+//     head.next = a;
+//     a.next = b;
 
 
-    int ans = NodeCount(head);
-    System.out.println(ans);
+//     int ans = NodeCount(head);
+//     System.out.println(ans);
 
 
   
 
 
      
-  }
+//   }
   
-}
+// }
