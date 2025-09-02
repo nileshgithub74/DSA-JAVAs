3dfd9708156ac60dd66d68464c2eba7ff71bb80c
Nilesh Kumar <kumarnilesh843127@gmail.com>
2025-09-02 13:13:10 +0530

    comit

--- Graph/GraphTraversal.java
+++ Graph/GraphTraversal.java
@@ -1,37 +1,27 @@
-// package Graph;
+import java.util.*;
 
-// import java.util.*;
+class Graph {
+    int V;
+    List<List<Integer>> adjlist;
 
-// public class GraphTraversal {
-//     static void dfs(int u, ArrayList<Integer>[] adj, boolean[] vis) {
-//         if (vis[u]) return; 
-//         vis[u] = true;
-//         System.out.print(u + " "); 
+    public Graph(int V) {
+        this.V = V;
+        adjlist = new ArrayList<>(V);
 
-//         for (int v : adj[u]) {
-//             dfs(v, adj, vis);
-//         }
-//     }
+        for (int i = 0; i < V; i++) {
+            adjlist.add(new ArrayList<>());
+        }
 
-//     public static void main(String[] args) {
-
+    }
 
 
-//         int n = 5; 
-//         ArrayList<Integer>[] adj = new ArrayList[n];
-//         for (int i = 0; i < n; i++) {
-//             adj[i] = new ArrayList<>();
-//         }
+    
+}
 
-        
-//         adj[0].add(1);
-//         adj[0].add(2);
-//         adj[1].add(3);
-//         adj[1].add(4);
+public class GraphTraversal {
 
-//         boolean[] vis = new boolean[n];
+    public static void main(String[] args) {
 
-//         System.out.println("DFS Traversal:");
-//         dfs(0, adj, vis); 
-//     }
-// }
+    }
+
+}
---
+++ LinkedList/LinkedListImplementation.java
@@ -0,0 +1,65 @@
+package LinkedList;
+
+class ListNode {
+    int data;
+    ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedListImplementation {
+
+    // Traversal of the LinkedList
+    public static void LinkedListTraversal(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + "-> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    // Reverse  Linked List
+    public static ListNode ReverseLinkedList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev; // new head
+    }
+
+    // Insert at Beginning
+    public static ListNode InsertAtBegin(ListNode head, int data) {
+        ListNode newNode = new ListNode(data);
+        newNode.next = head;
+        return newNode; 
+    }
+
+    public static void main(String[] args) {
+        ListNode head = null; 
+
+        head = InsertAtBegin(head, 10);
+        head = InsertAtBegin(head, 20);
+        head = InsertAtBegin(head, 40);
+        head = InsertAtBegin(head, 60);
+        head = InsertAtBegin(head, 70);
+
+        System.out.println("Original Linked List:");
+        LinkedListTraversal(head);
+
+        head = ReverseLinkedList(head);
+
+        System.out.println("Reversed Linked List:");
+        LinkedListTraversal(head);
+    }
+}
